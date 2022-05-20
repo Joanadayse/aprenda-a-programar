bf38c6439543d74616a17e6157eec1bd5358a428
Joana Dayse <101282171+Joanadayse@users.noreply.github.com>
2022-05-19 23:03:09 -0300

    criação do codigo js

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,15 @@
+var nome 
+var email
+var proficao 
+var idade 
+var lugar 
+
+nome="joana"
+email="daysejoanasetearrobagmailpontocom"
+proficao="autonoma"
+idade=30
+lugar="Paris"
+ 
+console.log (idade)
+
+
