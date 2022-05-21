055277a0ed2d3f61bfc981050b2ff14729c241e7
Joana Dayse <101282171+Joanadayse@users.noreply.github.com>
2022-05-21 16:41:52 -0300

    Update pratica-1.js

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,15 +1,11 @@
-var nome 
-var email
-var proficao 
-var idade 
-var lugar 
+let nome = "Joana"
+let email = "joana@dayse.com"
+let profissao = "autonoma"
+let idade = 30
+let viagemDosSonhos = "Paris"
 
-nome="joana"
-email="daysejoanasetearrobagmailpontocom"
-proficao="autonoma"
-idade=30
-lugar="Paris"
- 
-console.log (idade)
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
 
 
